1fc8575258222ac18e39531ae015c0738997364c
eli <eli.shagam@gmail.com>
2016-01-11 13:22:31 +0200

    args.h add doc

--- args.h
+++ args.h
@@ -15,10 +15,13 @@ extern int isPrefix(char const * p, char const * q);
 
 extern int isContain (char const *p, char c);
 
+// return 0 if not found; return 1 if found 
 extern int getBool (char const * name, int argc, const char * const argv[], char const * description);
 
+// return 0 if not found; return 1 if found; value in  res_value
 extern int getString (char const * param_name, int argc, char * argv[], char * res_value, char const * description) ;
 
+// returns -1 if not found; return value if found
 extern int getInteger (char const * param_name, int argc, const char * const argv[], char const * description);
 
 extern int split (char const * name_value, char * name, char * value);
